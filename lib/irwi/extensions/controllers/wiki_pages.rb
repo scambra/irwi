ca970bca1ccd07e9a7ac8782c7a892d1ec0f06d1
Sergio Cambra <sergio@programatica.es>
2017-03-22 15:07:04 +0100

    fix creating pages with / in path

--- lib/irwi/extensions/controllers/wiki_pages.rb
+++ lib/irwi/extensions/controllers/wiki_pages.rb
@@ -128,7 +128,7 @@ module Irwi::Extensions::Controllers::WikiPages
 
     # Initialize @page instance variable
     def setup_page
-      @page = page_class.find_by_path_or_new( params[:path] || '' ) # Find existing page by path or create new
+      @page = page_class.find_by_path_or_new(URI.unescape(params[:path] || '')) # Find existing page by path or create new
     end
 
     # Method, which called when user tries to visit
